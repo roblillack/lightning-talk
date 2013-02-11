8de4224eabb990c8212f8362e4f24b099ba49411
Robert Lillack <robert.lillack@ipoque.com>
2013-02-11 20:15:15 +0100

    Update to MonoBerry v0.1 libs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 // <versionNumber> + <buildId>
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
